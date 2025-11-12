db80ff7ae986b8922b040527856bb319d4df8de5
Nicholas <na.stewart365@gmail.com>
2025-11-12 13:56:36 -0500

    Class work

--- users.js
+++ users.js
@@ -10,4 +10,18 @@ router.get('/new-users', (req, res) => {
     res.send("New Users")
 })
 
+router.get('/:id', (req, res) => {
+    res.send(`Getting user data: ${res.params.id}`)
+}).delete((res, req) => {
+    res.send(`Deleting user data: ${res.params.id}`)
+}).put((res, res) => {
+    res.send(`Updating user data: ${res.params.id}`)
+})
+
+router.param("id", (req, res, next, id) => {
+    console.log(`Accessing user #${id}`)
+    next();
+})
+
+
 module.exports = router;
